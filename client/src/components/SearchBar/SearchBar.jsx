40f127fdd0e7bc5716677689a013f9f62391f375
anfevera <116735342+anfevera97@users.noreply.github.com>
2023-05-19 16:51:27 -0500

    .

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,34 +1,3 @@
-// import React, { useState } from 'react';
-// import { onSearch } from '../../redux/actions';
-// import { useDispatch, useSelector } from 'react-redux';
-
-// const SearchBar = () => {
-//     const [query, setQuery] = useState('');
-// 	const filter = useSelector((state) => state.filteredDogs);
-// 	const dispatch = useDispatch();
-
-// 	const handleChange = (event) => {
-// 		setQuery(event.target.value);
-// 	};
-
-// 	return (
-// 		<div>
-// 			<input
-// 				type='text'
-// 				placeholder='Search Dog Breed'
-// 				value={query}
-// 				onChange={handleChange}
-// 				onKeyDown={(event) =>
-// 					event.key === 'Enter' && dispatch(onSearch(query))
-// 				}
-// 			/>
-// 			<button onClick={() => dispatch(onSearch(query))}>Search</button>
-// 		</div>
-// 	);
-// };
-
-// export default SearchBar;
-
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { onSearch } from '../../redux/actions';
